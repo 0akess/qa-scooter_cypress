a2abf41c96cec24e1b749ecea0c97ef4deee6b97
Daniil Samsonenko <samsonenkodan@yandex.ru>
2022-04-28 19:54:33 +0400

    fix POM

---
+++ cypress/integration/TrackPage_TestsSuite.ts
@@ -0,0 +1,29 @@
+import {TrackPage} from "../pageObject/OrderTrackPage";
+
+it('Проверка поиска заказа со страницы Track с невалидным номером', () => {
+
+    TrackPage
+        .openTrackPage()
+        .inputSearchNumberOrderInOrderStatusPage('sdfsd')
+        .buttonWatchSearchNumberOrder()
+        .imgNotFound();
+});
+
+it('Проверка поиска заказа со страницы Maine с невалидным номером', () => {
+
+    TrackPage
+        .openMainePage()
+        .buttonOrderStatus()
+        .inputSearchNumberOrderInHeader('234')
+        .buttonGoSearchNumberOrder()
+        .imgNotFound();
+});
+
+it('Проверка поиска заказа со страницы Track с невалидным номером', () => {
+
+    TrackPage
+        .openTrackPage()
+        .inputSearchNumberOrderInOrderStatusPage('1231')
+        .buttonWatchSearchNumberOrder()
+        .imgNotFound();
+});
